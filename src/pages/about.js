bf03c1987eaf136a93328d238fd55925f80d901d
张晓渊 <zhangxiaoyuan@itcast.cn>
2024-01-26 11:54:32 +0800

    feat: init

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <div>about me</div>
+  </Layout>
+)
+
+export default About
---
+++ src/pages/news/index.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import "./index.css"
+
+import Layout from "@components/layout"
+
+const News = () => {
+  const { directus } = useStaticQuery(graphql`{
+    directus {
+      cars {
+        id
+        name
+        type
+        pic {
+          id
+        }
+      }
+    }
+  }`)
+  console.log('directus', directus);
+  return (
+    <Layout>
+      {
+        directus.cars.map(car => (
+          <div key={car.id} className="car-item">
+            <span>{car.name}</span>
+            <img
+              className="car-pic"
+              src={'//directus-t.itcast.cn/assets/' + car.pic.id}
+              alt=""
+            />
+          </div>
+        ))
+      }
+    </Layout> 
+  )
+}
+  
+
+
+export default News
